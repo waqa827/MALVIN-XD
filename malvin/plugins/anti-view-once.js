446afdf44e977c5b507b2817107d9aa8664ad23b
Malvin King <malvink003@gmail.com>
2025-02-28 10:48:58 +0200

    Add files via upload

---
+++ malvin/plugins/anti-view-once.js
@@ -0,0 +1,59 @@
+/*
+
+ANYWAY, YOU MUST GIVE CREDIT TO MY CODE WHEN COPY IT
+CONTACT ME HERE +263714757857
+YT: Malvin King Tech
+Github: kingmalvn 
+*/
+
+const axios = require('axios');
+const config = require('../../settings');
+const { cmd, commands } = require('../command');
+
+const fs = require("fs");
+
+cmd({
+    pattern: "vv",
+    react: "💾",
+    alias: ["retrive", "viewonce"],
+    desc: "Fetch and resend a ViewOnce message content (image/video/voice).",
+    category: "misc",
+    use: "<query>",
+    filename: __filename
+}, async (conn, mek, m, { from, reply }) => {
+    try {
+        if (!m.quoted) return reply("Please reply to a ViewOnce message.");
+
+        const mime = m.quoted.type;
+        let ext, mediaType;
+        
+        if (mime === "imageMessage") {
+            ext = "jpg";
+            mediaType = "image";
+        } else if (mime === "videoMessage") {
+            ext = "mp4";
+            mediaType = "video";
+        } else if (mime === "audioMessage") {
+            ext = "mp3";
+            mediaType = "audio";
+        } else {
+            return reply("Unsupported media type. Please reply to an image, video, or audio message.");
+        }
+
+        var buffer = await m.quoted.download();
+        var filePath = `${Date.now()}.${ext}`;
+
+        fs.writeFileSync(filePath, buffer); 
+
+        let mediaObj = {};
+        mediaObj[mediaType] = fs.readFileSync(filePath);
+
+        await conn.sendMessage(m.chat, mediaObj);
+
+        fs.unlinkSync(filePath);
+
+    } catch (e) {
+        console.log("Error:", e);
+        reply("An error occurred while fetching the ViewOnce message.", e);
+    }
+});
